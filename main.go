867358218076ebdd96a99b3cd3425e597e630454
aidocons <consultant-butlyai@aidodev.com>
2025-05-18 16:00:16 +0100

    plugin hotfix

--- main.go
+++ main.go
@@ -1,27 +1,35 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 
+	"github.com/hashicorp/go-plugin"
 	shared "github.com/pigen-dev/shared"
 	"github.com/pigen-plugins/google-cloud-run/pkg"
 )
 
 func main() {
-	plugin := shared.Plugin{
-		Label: "google-cloud-run",
-		Config: map[string]any{
-			"project_id":   "aidodev",
-			"location":     "europe-west1",
-			"service_name": "your-service-name",
-			"unauthenticated": true,
-		},
-	}
-	cr := pkg.GoogleCloudRun{}
-	err := cr.SetupPlugin(plugin)
-	if err != nil {
-		fmt.Println("Error setting up plugin:", err)
-	}
-	output := cr.GetOutput(plugin)
-	fmt.Println("Cloud Run URL:", output)
+	// plugin := shared.Plugin{
+	// 	Label: "google-cloud-run",
+	// 	Config: map[string]any{
+	// 		"project_id":   "aidodev",
+	// 		"location":     "europe-west1",
+	// 		"service_name": "your-service-name",
+	// 		"unauthenticated": true,
+	// 	},
+	// }
+	// cr := pkg.GoogleCloudRun{}
+	// err := cr.SetupPlugin(plugin)
+	// if err != nil {
+	// 	fmt.Println("Error setting up plugin:", err)
+	// }
+	// output := cr.GetOutput(plugin)
+	// fmt.Println("Cloud Run URL:", output)
+	cr := &pkg.GoogleCloudRun{}
+	pluginMap := map[string]plugin.Plugin{"pigenPlugin": &shared.PigenPlugin{Impl: cr}}
+
+	plugin.Serve(&plugin.ServeConfig{
+		HandshakeConfig: shared.Handshake,
+		Plugins:         pluginMap,
+	})
 }
